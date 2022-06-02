4a1d1311f5a8e92ae6e77ed28123b1da8166d1eb
Knut Leborg <knut@lepa.no>
2022-06-02 15:28:11 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,15 +3,10 @@
 namespace Knutle\ShellExec\Tests;
 
 use Knutle\ShellExec\ShellExecServiceProvider;
-use Knutle\TestStubs\InteractsWithStubs;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Spatie\Snapshots\MatchesSnapshots;
 
 class TestCase extends Orchestra
 {
-    use InteractsWithStubs;
-    use MatchesSnapshots;
-
     protected function getPackageProviders($app): array
     {
         return [
