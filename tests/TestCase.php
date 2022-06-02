b855d59c0f8dac820bde8a0c3c331e6b9b8ee40a
Knut Leborg <knut@lepa.no>
2022-06-02 15:18:44 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,6 @@ namespace Knutle\ShellExec\Tests;
 use Knutle\ShellExec\ShellExecServiceProvider;
 use Knutle\TestStubs\InteractsWithStubs;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Pest\TestSuite;
-use ReflectionClass;
 use Spatie\Snapshots\MatchesSnapshots;
 
 class TestCase extends Orchestra
@@ -19,21 +17,5 @@ class TestCase extends Orchestra
         return [
             ShellExecServiceProvider::class,
         ];
-    }
-
-    protected function getSnapshotDirectory(): string
-    {
-        return implode(DIRECTORY_SEPARATOR, [
-            TestSuite::getInstance()->rootPath,
-            'tests',
-            '__snapshots__',
-        ]);
-    }
-
-    protected function getSnapshotId(): string
-    {
-        return (new ReflectionClass($this))->getShortName().'__'.
-            str_replace(' ', '_', $this->getName()).'__'.
-            $this->snapshotIncrementor;
     }
 }
